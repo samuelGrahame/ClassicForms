a1723d8f70e94f892534c3310c3d598b3fb8c5db
samuel grahame <samuel@intelogy.com.au>
2018-04-05 11:43:29 +1000

    set location = 0,0 on load. added borderstyle and control box setting for form

--- ClassicForms/Windows/Forms/Form.cs
+++ ClassicForms/Windows/Forms/Form.cs
@@ -10,28 +10,185 @@ namespace System.Windows.Forms
 {
     public class Form : ContainerControl
     {
-        private static int FormTopBorder = 27;
-        private static int FormBottonBorder = 6;
-        private static int FormLeftBorder = 6;
-        private static int FormRightBorder = 6;
-        
+        private int FormTopBorder = 31;
+        private int FormBottonBorder = 1;
+        private int FormLeftBorder = 1;
+        private int FormRightBorder = 1;
+        private bool allowSizeChange = true;
+
         public Form() : base()
         {            
             Element.setAttribute("scope", "form");
             
             TabStop = false;
+
+            this.Location = new Point(0, 0);
+
+            _setBorderWidth();
         }
 
+        private bool _controlBox = true;
+
+        public bool ControlBox
+        {
+            get { return _controlBox; }
+            set {
+                if(_controlBox != value)
+                {
+                    _controlBox = value;
+                    _processWinFormView();
+                }                
+            }
+        }
+
+        private FormBorderStyle _formBorderStyle;
+
+        public FormBorderStyle FormBorderStyle
+        {
+            get { return _formBorderStyle; }
+            set {
+                if(_formBorderStyle != value)
+                {
+                    _formBorderStyle = value;
+                    _processWinFormView();
+                }                
+            }
+        }
+
+        private void _processWinFormView()
+        {
+            var clientSize = ClientSize;
+            // need to allow for custom size per style - currently set as windows 10.
+            switch (_formBorderStyle)
+            {
+                case FormBorderStyle.None:
+                    FormTopBorder = 0;
+                    FormBottonBorder = 0;
+                    FormLeftBorder = 0;
+                    FormRightBorder = 0;
+                    allowSizeChange = false;
+
+                    break;
+                case FormBorderStyle.FixedSingle:
+                    allowSizeChange = false;
+                    if (!ControlBox)
+                    {
+                        FormTopBorder = 1;
+                    }
+                    else
+                    {
+                        FormTopBorder = 31;
+                    }                        
+                    FormBottonBorder = 1;
+                    FormLeftBorder = 1;
+                    FormRightBorder = 1;
+                    break;
+                case FormBorderStyle.Fixed3D:
+                    allowSizeChange = false;
+                    if (!ControlBox)
+                    {
+                        FormTopBorder = 3;
+                        FormBottonBorder = 3;
+                        FormLeftBorder = 3;
+                        FormRightBorder = 3;
+                    }
+                    else
+                    {
+                        FormTopBorder = 31;
+                        FormBottonBorder = 1;
+                        FormLeftBorder = 1;
+                        FormRightBorder = 1;
+                    }
+                    break;
+                case FormBorderStyle.FixedDialog:
+
+                    allowSizeChange = false;
+                    if (!ControlBox)
+                    {
+                        FormTopBorder = 2;
+                        FormBottonBorder = 2;
+                        FormLeftBorder = 2;
+                        FormRightBorder = 2;
+                    }
+                    else
+                    {
+                        FormTopBorder = 31;
+                        FormBottonBorder = 1;
+                        FormLeftBorder = 1;
+                        FormRightBorder = 1;
+                    }
+
+                    
+                    break;
+                case FormBorderStyle.Sizable:
+                    if(!ControlBox)
+                    {
+                        FormTopBorder = 8;
+                    }
+                    else
+                    {
+                        FormTopBorder = 31;
+                    }
+                    FormBottonBorder = 1;
+                    FormLeftBorder = 1;
+                    FormRightBorder = 1;
+                    allowSizeChange = true;
+                    break;
+                case FormBorderStyle.FixedToolWindow:
+                    if (!ControlBox)
+                    {
+                        FormTopBorder = 1;
+                    }
+                    else
+                    {
+                        FormTopBorder = 31;
+                    }
+                    FormBottonBorder = 0;
+                    FormLeftBorder = 0;
+                    FormRightBorder = 0;
+                    allowSizeChange = false;
+                    break;
+                case FormBorderStyle.SizableToolWindow:
+                    if (!ControlBox)
+                    {
+                        FormTopBorder = 8;
+                    }
+                    else
+                    {
+                        FormTopBorder = 31;
+                    }
+                    FormBottonBorder = 1;
+                    FormLeftBorder = 1;
+                    FormRightBorder = 1;
+                    allowSizeChange = true;
+                    break;
+                default:
+                    break;
+            }
+
+            _setBorderWidth();
+
+            ClientSize = clientSize;
+        }
+
+        private void _setBorderWidth()
+        {
+            Element.style.borderTopWidth = FormTopBorder + "px";
+            Element.style.borderBottomWidth = FormBottonBorder + "px";
+            Element.style.borderLeftWidth = FormLeftBorder + "px";
+            Element.style.borderRightWidth = FormRightBorder + "px";
+        }
+        
         public override Font Font { get { return base.Font; } set {                
                 base.Font = value;                
-            } }        
+            } }
 
-        public static Size GetClientSize(Size size)
+        private Size GetClientSize(Size size)
         {
             return new Size(size.Width - (FormLeftBorder + FormRightBorder), size.Height - (FormTopBorder + FormBottonBorder));
         }
 
-        public static Size SetSize(Size clientSize)
+        private Size SetSize(Size clientSize)
         {
             return new Size(clientSize.Width + (FormLeftBorder + FormRightBorder), clientSize.Height + (FormTopBorder + FormBottonBorder));
         }
