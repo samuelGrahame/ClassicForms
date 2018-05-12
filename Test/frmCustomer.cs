3206672c8a186e2686cbc11ffbd60bc25b4ff2b7
samfromdeath <samuel@intelogy.com.au>
2018-05-12 10:03:37 +1000

    Added Anchor Demo

--- Test/frmCustomer.cs
+++ Test/frmCustomer.cs
@@ -12,6 +12,9 @@ namespace Test
         private Button button2;
         private Button button3;
         private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
         private Button button1;
 
         public frmCustomer() : base()
@@ -25,12 +28,15 @@ namespace Test
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button1.Location = new System.Drawing.Point(274, 188);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(267, 237);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(160, 55);
             this.button1.TabIndex = 0;
@@ -41,8 +47,8 @@ namespace Test
             // 
             // button2
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button2.Location = new System.Drawing.Point(274, 127);
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(267, 176);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(160, 55);
             this.button2.TabIndex = 1;
@@ -55,17 +61,18 @@ namespace Test
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(529, 12);
+            this.button3.Location = new System.Drawing.Point(601, 12);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(156, 389);
+            this.button3.Size = new System.Drawing.Size(84, 389);
             this.button3.TabIndex = 2;
             this.button3.Tag = "btn btn-primary";
-            this.button3.Text = "Anchor Right";
+            this.button3.Text = "Anchor Top, Bottom, Right";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 12);
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Location = new System.Drawing.Point(267, 115);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(160, 55);
             this.button4.TabIndex = 3;
@@ -74,9 +81,48 @@ namespace Test
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(102, 336);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(493, 65);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "btn btn-primary";
+            this.button5.Text = "Anchor Bottom, Left, Right";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.button6.Location = new System.Drawing.Point(12, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(84, 389);
+            this.button6.TabIndex = 5;
+            this.button6.Tag = "btn btn-primary";
+            this.button6.Text = "Anchor Top, Bottom, Left";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Location = new System.Drawing.Point(102, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(493, 65);
+            this.button7.TabIndex = 6;
+            this.button7.Tag = "btn btn-primary";
+            this.button7.Text = "Anchor Top, Left, Right";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
             // frmCustomer
             // 
             this.ClientSize = new System.Drawing.Size(697, 413);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
