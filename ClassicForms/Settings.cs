ecc1d62db757818a0785deaad32f259d2888826f
samuel grahame <samuel@intelogy.com.au>
2018-05-30 15:40:36 +1000

    added is using x type

--- ClassicForms/Settings.cs
+++ ClassicForms/Settings.cs
@@ -7,7 +7,7 @@ using static Retyped.dom;
 
 namespace System
 {
-    public static class Settings
+    public class Settings
     {
         public static bool IsEdge;
         public static bool IsFF;
@@ -45,25 +45,70 @@ namespace System
             Right
         }
 
-
+        private static bool _isUsingWindowsCSS;
         /// <summary>
         /// check in the window10.css is in the current html file. window10.css is a classicforms default css file.
         /// </summary>
         /// <returns></returns>
         public static bool IsUsingWindowsCSS()
         {
+            if (_hasLoaded)
+                return _isUsingWindowsCSS;
+            return ContainsCssLink("windows10.css");            
+        }
+
+        public static bool ContainsCssLink(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                return false;
+
+            content = content.Trim().ToLower();
             for (int i = 0; i < document.head.childElementCount; i++)
             {
                 var child = document.head.children[i];
                 if (child is HTMLLinkElement)
                 {
                     var link = child.As<HTMLLinkElement>();
-                    if (link.rel != null && link.rel.ToLower() == "stylesheet" && link.href != null && link.href.ToLower().Contains("windows10.css"))
+                    if (link.rel != null && link.rel.ToLower() == "stylesheet" && link.href != null && link.href.ToLower().Contains(content))
                         return true;
                 }
-            }            
+            }
 
             return false;
+        }
+
+        private static bool _isUsingBootStrap;
+        public static bool IsUsingBootStrap()
+        {
+            if (_hasLoaded)
+                return _isUsingBootStrap;
+            return ContainsCssLink("bootstrap");
+        }
+
+        private static bool _isUsingMaterial;
+        public static bool IsUsingMaterial()
+        {
+            if (_hasLoaded)
+                return _isUsingMaterial;
+            return ContainsCssLink("material");
+        }
+
+        private static bool _hasLoaded;
+
+        static Settings()
+        {
+            try
+            {
+                _isUsingMaterial = IsUsingMaterial();
+                _isUsingBootStrap = IsUsingBootStrap();
+                _isUsingWindowsCSS = IsUsingWindowsCSS();
+            }
+            catch (Exception)
+            {
+
+            }
+            
+            _hasLoaded = true;
         }
     }
 }
